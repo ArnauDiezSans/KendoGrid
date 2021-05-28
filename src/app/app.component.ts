2e88166a9c05842690306065cd7e5271a14ad9bf
Arnau Díez <arnau.diez@grupocastilla.es>
2021-05-28 13:15:13 +0200

    commit sort page filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import * as data from './puestos.json';
+import { GridDataResult } from "@progress/kendo-angular-grid";
+import { ButtonsModule } from "@progress/kendo-angular-buttons";
+import { GridItem } from '@progress/kendo-angular-grid';
+import { State, process } from '@progress/kendo-data-query';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,7 +14,28 @@ import * as data from './puestos.json';
 
 export class AppComponent {
 
-  public gridData: any[] = (data as any).default;
+  public gridView: GridDataResult;
+  public dataPuestos: any[] = (data as any).default;
   title = 'KendoArnau';
-}
 
+
+  public state: State =
+    {
+      skip: 0,
+      take: 15,
+    }
+    ;
+
+  constructor() {
+    this.loadPuestos();
+  }
+
+  private loadPuestos(): void {
+    this.gridView = process(this.dataPuestos, this.state);
+  }
+
+  public dataStateChange(state): void {
+    this.state = state;
+    this.loadPuestos();
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GridModule } from '@progress/kendo-angular-grid';
-
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, GridModule],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, GridModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
