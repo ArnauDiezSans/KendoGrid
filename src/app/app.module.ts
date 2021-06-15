0f527c454ddbb5c47c14bbaf3a34093dd66973f2
Epsilon Prog. Net <epsilon.prget@grupocastilla.es>
2021-06-15 13:31:39 +0200

    falta add en responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,17 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ExcelModule } from '@progress/kendo-angular-grid';
 import {MatMenuModule} from '@angular/material/menu';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule} from '@angular/material/card';
+import {  MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { DatePickerModule } from '@progress/kendo-angular-dateinputs';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, GridModule, HttpClientModule, ExcelModule, MatMenuModule, FlexLayoutModule],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, GridModule, HttpClientModule, ExcelModule, MatMenuModule, FlexLayoutModule, MatCardModule, MatFormFieldModule, ReactiveFormsModule, MatInputModule, MatSlideToggleModule, MatDatepickerModule, DatePickerModule, MatNativeDateModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
